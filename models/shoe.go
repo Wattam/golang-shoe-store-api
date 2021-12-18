51747a822beeddbf429fa58970a2d2b69b8b7137
Nalbert Wattam <nalbertwattam@gmail.com>
2021-12-18 18:39:39 -0300

    JSON tags to hide GORM fields

--- models/shoe.go
+++ models/shoe.go
@@ -13,7 +13,7 @@ type Shoe struct {
 	Colour    string         `json:"colour"`
 	Material  string         `json:"material"`
 	Price     float64        `json:"price,string"`
-	CreatedAt time.Time      `json:"created"`
-	UpdatedAt time.Time      `json:"updated"`
-	DeletedAt gorm.DeletedAt `json:"deleted" gorm:"index"`
+	CreatedAt time.Time      `json:"-" gorm:"<-:create"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
